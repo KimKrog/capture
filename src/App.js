61993d6b681a3be92c620c6eaebf4ccff48c0dc4
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-03-24 22:46:55 +0100

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AboutUs from "./pages/AboutUs";
 import Globalstyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <Globalstyle />
+      <Nav />
       <AboutUs />
     </div>
   );

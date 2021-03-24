0abed33a2e506be1362000bf9d5cb6e7e9003e50
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-03-24 22:06:34 +0100

    Styled components setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import AboutUs from "./pages/AboutUs";
+import Globalstyle from "./components/GlobalStyle";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <Globalstyle />
       <AboutUs />
     </div>
   );

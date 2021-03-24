425e76ec11e791ce1b08065f46231e6b41ae7a20
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-03-24 22:32:46 +0100

    Style FAQ

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,9 @@
+import styled from "styled-components";
+import { Layout } from "../styles";
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any questions <span>FAQ</span>
       </h2>
@@ -13,6 +16,7 @@ const FaqSection = () => {
             placeat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily schedule</h4>
@@ -33,6 +37,7 @@ const FaqSection = () => {
             placeat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer</h4>
@@ -43,9 +48,43 @@ const FaqSection = () => {
             placeat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
 
 export default FaqSection;
+
+const Faq = styled(Layout)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0;
+
+    p {
+      padding: 1rem 0;
+    }
+  }
+`;

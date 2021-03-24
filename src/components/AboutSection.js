6ac023845e80627ad6e679e405ec614eeb5dc094
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-03-24 22:25:11 +0100

    Tweak Service section + Add fonts

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,10 @@
 import home1 from "../img/home1.png";
 import styled from "styled-components";
+import { Layout, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
-    <About>
+    <Layout>
       <Description>
         <div className="title">
           <Hide>
@@ -27,41 +28,8 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="Guy with camera" />
       </Image>
-    </About>
+    </Layout>
   );
 };
 
 export default AboutSection;
-
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
